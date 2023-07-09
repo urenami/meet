385bec03d99479c3ec15e40c45101f046713c5a1
urenami <122377715+urenami@users.noreply.github.com>
2023-07-08 23:25:00 -0400

    task 4.4

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import EventList from './EventList';
+import CitySearch from './CitySearch';
+import NumberOfEvents from './NumberOfEvents';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-           <code>Hello World!</code> 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    events: [],
+    locations: [],
+    numberOfEvents: 10
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <CitySearch />
+        <NumberOfEvents />
+        <EventList />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/__tests__/EventList.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Event from '../Event'; 
+import EventList from '../EventList';
+import { mockData } from '../mock-data';
+
+describe('<EventList /> component', () => {
+    test('render correct number of events', () => {
+        const EventListWrapper = shallow(<EventList events={mockData} />);
+        expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
+      });
+
+});

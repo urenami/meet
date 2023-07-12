dd2dd6519d510adce580c75ca68828db91221fd2
urenami <122377715+urenami@users.noreply.github.com>
2023-07-12 10:57:06 -0400

    updated files

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Event from '../Event'; 
-import EventList from '../EventList';
-import { mockData } from '../mock-data';
+import React from "react";
+import { shallow } from "enzyme";
+import Event from "../Event";
+import EventList from "../EventList";
+import { mockData } from "./mock-data";
 
-describe('<EventList /> component', () => {
-    test('render correct number of events', () => {
-        const EventListWrapper = shallow(<EventList events={mockData} />);
-        expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
-      });
-
+describe("<EventList /> component", () => {
+  test("render correct number of events", () => {
+    const EventListWrapper = shallow(<EventList events={mockData} />);
+    expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
+  });
 });
